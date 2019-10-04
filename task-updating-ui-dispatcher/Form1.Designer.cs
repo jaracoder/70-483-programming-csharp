597158b4c7e9348fde6310e00ad738e259201909
Juan A. Ripoll <juan.ripoll.93@gmail.com>
2019-10-04 18:42:52 +0200

    Añade proyectos de tareas y threads

---
+++ task-updating-ui-dispatcher/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace task_updating_ui_dispatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Media aritmética aleatoria";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de valores:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(133, 68);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(31, 112);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 3;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.BtnEmpezar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(31, 155);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 219);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnEmpezar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Media aritmética aleatoria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ task-updating-ui-dispatcher/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_updating_ui_dispatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double calcularMedias(long numValores)
+        {
+            double total = 0;
+            Random rand = new Random();
+            for (long valores = 0; valores < numValores; valores++)
+            {
+                total = total + rand.NextDouble();
+            }
+            return total / numValores;
+        }
+
+        private Task<double> asyncCalcularMedias(long numValores)
+        {
+            return Task.Run(() =>
+            {
+                return calcularMedias(numValores);
+            });
+        }
+
+        private async void BtnEmpezar_Click(object sender, EventArgs e)
+        {
+            long numValores = long.Parse(txtResultado.Text);
+
+            double resultado = await asyncCalcularMedias(numValores);
+            lblResultado.Text = $"Resultado: {resultado}";
+
+            // Ejecuta método en tarea para no bloquear la UI
+            /*Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+            Task.Run(() =>
+            {
+                double result = calcularMedias(numValores);
+
+                // En Windows Forms podemos utilizar el dispatcher del hilo de la interfaz de usuario
+                dispatcher.BeginInvoke(new Action(() => {
+                    lblResultado.Text = $"Resultado: {result}";
+                }));
+
+                // En aplicaciones universales de Winodws o WPF sería así:
+                /*lblResultado.Dispatcher.RunAsync(CoreDispatcherPriority.Normal
+                {
+                    lblResultado.Text = $"Resultado: {result}";
+                });*/
+            // });
+
+            // Con valores altos bloquea la interfaz de usuario..
+            //lblResultado.Text = $"Resultado: {calcularMedias(numValores)}";
+
+        }
+    }
+}
