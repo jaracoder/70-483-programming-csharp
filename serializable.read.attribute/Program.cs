17b28085ca78aab468a78c47259230fc612b2590
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-07 13:09:18 +0200

    Añade proyectos de serializable y reflection.

---
+++ serializable.read.attribute/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace serializable.read.attribute
+{
+    class Program
+    {
+        [AttributeUsage(AttributeTargets.Class)] // Define donde se puede utilizar el atributo
+        public class AtributoProgramador : Attribute
+        {
+            private string programador;
+
+            public AtributoProgramador(string programador)
+            {
+                this.programador = programador;
+            }
+
+            public string Programador
+            {
+                get
+                {
+                    return programador;
+                }
+            }
+        }
+
+
+        [AtributoProgramador("jripoll.dev")]
+        public class Persona
+        {
+            public string Nombre;
+        }
+
+
+        static void Main(string[] args)
+        {
+            AtributoProgramador a = (AtributoProgramador) Attribute.GetCustomAttribute(typeof(Persona), typeof(AtributoProgramador));
+            Console.WriteLine("El programador encargado de la clase persona es: {0}", a.Programador);
+            Console.ReadKey();
+        }
+    }
+}
