c45d1a0a62bf659b467feed985bd169c22fd63c3
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-07 11:22:49 +0200

    Añade proyectos de Enumerables, Disposable y Yield.

---
+++ idisposable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace idisposable
+{
+    class Program
+    {
+        class ObjetoDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Se libera la memoria del objeto");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (ObjetoDisposable obj = new ObjetoDisposable())
+            {
+                Console.WriteLine("Empieza el bloque del ObjetoDisposable");
+            }
+            Console.ReadKey();
+        }
+    }
+}
