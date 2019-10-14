0e2472bb8bc01d27846738156a3cd008617c6d83
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-14 12:55:25 +0200

    Sube proyectos de cadenas.

---
+++ strings.formats/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace strings.formats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 99;
+            double pi = 3.141592654;
+
+            Console.WriteLine("{0, -10:D}{0, -10:X}{1,5:N2}", i, pi);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ strings.iformattable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace strings.iformattable
+{
+    class Program
+    {
+        class PistaMusica : IFormattable
+        {
+            string Artista { get; set; }
+            string Titulo { get; set; }
+
+            public PistaMusica(string artista, string titulo)
+            {
+                Artista = artista;
+                Titulo = titulo;
+            }
+
+            public string ToString(string format, IFormatProvider formatProvider)
+            {
+                if (string.IsNullOrWhiteSpace(format))
+                    format = "G";
+
+                switch (format)
+                {
+                    case "A": return Artista;
+                    case "T": return Titulo;
+                    case "G":
+                    case "F": return Artista + " " + Titulo;
+                    default:
+                        throw new FormatException("El formato especificado no es válido.");
+                }
+            }
+
+            public override string ToString()
+            {
+                return Artista + " " + Titulo;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            PistaMusica pista = new PistaMusica("Juan A. Ripoll", "My Way");
+            Console.WriteLine("Pista: {0:F}", pista);
+            Console.WriteLine("Artista: {0:A}", pista);
+            Console.WriteLine("Título: {0:T}", pista);
+            Console.ReadKey();
+        }
+    }
+}
