ac774beaad10cb385d46f3dbb633b304b0b5c99b
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-14 17:10:11 +0200

    Sube proyectos de json y xml.

---
+++ json.create/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace json.create
+{
+    class Program
+    {
+        class PistaMusica
+        {
+            public string Artista { get; set; }
+            public string Titulo { get; set; }
+            public int Longitud { get; set; }
+
+            public PistaMusica(string artista, string titulo, int longitud)
+            {
+                Artista = artista;
+                Titulo = titulo;
+                Longitud = longitud;
+            }
+
+            public override string ToString()
+            {
+                return Artista + " " + Titulo + " " + Longitud.ToString() + " segundos";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PistaMusica pista = new PistaMusica(
+                artista: "Juanito", titulo: "My Way", longitud: 240);
+
+            // Serializa JSON del objeto PistaMusica
+            string json = JsonConvert.SerializeObject(pista);
+            Console.WriteLine("JSON Serializado:\n" + json);
+
+            // Deserializa JSON del objeto PistaMusica
+            PistaMusica aux = JsonConvert.DeserializeObject<PistaMusica>(json);
+            Console.WriteLine("JSON Deserializado:\n" + aux);
+
+            // Lista de pistas
+            List<PistaMusica> album = new List<PistaMusica>();
+            string[] nombresPistas = new[] { "My Way", "Your Way", "Their Way", "The Wrong Way" };
+            foreach (string nombre in nombresPistas)
+            {
+                album.Add(new PistaMusica("Juanito", nombre, 230));
+            }
+
+            // Serializa lista de objetos
+            string jsonArray = JsonConvert.SerializeObject(album);
+            Console.WriteLine("Array JSON Serializado:\n" + jsonArray);
+
+            // Deserializa lista de objetos
+            List<PistaMusica> auxArray = JsonConvert.DeserializeObject<List<PistaMusica>>(jsonArray);
+            Console.WriteLine("Array JSON Deserializado: ");
+            foreach (PistaMusica p in auxArray)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ xml.create/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace xml.create
+{
+    public class PistaMusica
+    {
+        public string Artista { get; set; }
+        public string Titulo { get; set; }
+        public int Longitud { get; set; }
+
+        public PistaMusica()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Artista + " " + Titulo + " " + Longitud.ToString() + " segundos";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PistaMusica pista = new PistaMusica() { Artista = "Juanito", Titulo = "My Way", Longitud = 320 };
+
+            XmlSerializer serializador = new XmlSerializer(typeof(PistaMusica));
+
+            TextWriter escritor = new StringWriter();
+            serializador.Serialize(textWriter: escritor, o: pista);
+            escritor.Close();
+
+            string pistaXML = escritor.ToString();
+            Console.WriteLine("Pista serializada en XML:\n" + pistaXML);
+
+            TextReader lector = new StringReader(pistaXML);
+            PistaMusica aux = serializador.Deserialize(lector) as PistaMusica;
+            Console.WriteLine("Pista deserializada en XML:\n" + aux);
+
+
+            Console.ReadKey();
+        }
+    }
+}
