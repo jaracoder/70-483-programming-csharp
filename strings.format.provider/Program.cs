f88ba7aa72101ba76c86fbab6e521e198aecf8f5
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-14 13:39:35 +0200

    Añade proyectos de cadenas.

---
+++ strings.format.provider/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace strings.format.provider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 255.55;
+
+            CultureInfo proveedorEN = new CultureInfo("en-US");
+            Console.WriteLine("Mi saldo en América es de {0}", saldo.ToString("C", proveedorEN));
+
+            CultureInfo proveedorUK = new CultureInfo("en-GB");
+            Console.WriteLine("Mi saldo en Londres es de {0}", saldo.ToString("C", proveedorUK));
+
+            CultureInfo proveedorES = new CultureInfo("es-ES");
+            Console.WriteLine("Mi saldo en España es de {0}", saldo.ToString("C", proveedorES));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ strings.interpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace strings.interpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Juan";
+            int edad = 25;
+
+            Console.WriteLine("Mi nombre es {0} y tengo {1, -5:D} años.", nombre, edad);
+            Console.WriteLine(string.Format("Mi nombre es {0} y tengo {1, -5:D} años.", nombre, edad));
+
+            Console.WriteLine($"Mi nombre es {nombre} y tengo {edad, -5:D} años.");
+            Console.WriteLine(string.Format( $"Mi nombre es {nombre} y tengo {edad,-5:D} años."));
+
+            Console.ReadKey();
+        }
+    }
+}
