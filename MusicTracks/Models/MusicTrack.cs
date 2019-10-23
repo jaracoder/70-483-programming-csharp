acc3751bf667906d2702bd4558989ab485d1e14f
Juan Antonio Ripoll <juan.ripoll.93@gmail.com>
2019-10-23 09:44:00 +0200

    Añade proyecto web de MusicTracks.

---
+++ MusicTracks/Models/MusicTrack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicTracks.Models
+{
+    public class MusicTrack
+    {
+        public int ID { get; set; }
+        public string Artist { get; set; }
+        public int Title { get; set; }
+        public int Lenght { get; set; }
+    }
+}
